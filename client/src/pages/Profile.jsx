5ff78cdec67c9a465116d54ed2f474574f8fd800
sadyplabon <plabon.pen@gmail.com>
2024-01-01 21:57:21 +0600

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -121,6 +121,23 @@ export default function Profile() {
       showListingsError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -171,7 +188,7 @@ export default function Profile() {
           </Link>
           <div className='flex flex-col item-center'>
             <button className='text-green-700 uppercase'>Edit</button>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={()=>handleDeleteListing(listing._id)} className='text-red-700 uppercase'>Delete</button>
           </div>
         </div>
       ))
