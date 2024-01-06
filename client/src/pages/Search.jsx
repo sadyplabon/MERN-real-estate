b1e39e3472da97aa3ec48e11dabf2a7a579078c7
sadyplabon <plabon.pen@gmail.com>
2024-01-06 21:51:46 +0600

    add show more functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,8 +15,7 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-
-console.log(listings);
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -50,9 +49,15 @@ console.log(listings);
 
             const fetchListings = async () => {
                 setLoading(true);
+                setShowMore(false);
                 const searchQuery = urlParams.toString();
                 const res = await fetch(`/api/listing/get?${searchQuery}`);
                 const data = await res.json();
+                if(data.length > 8) {
+                    setShowMore(true);
+                } else {
+                    setShowMore(false);
+                }
                 setListings(data);
                 setLoading(false);
             };
@@ -90,6 +95,20 @@ console.log(listings);
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    };
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = res.json();
+        if (data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+
     };
     return (
         <div className="flex flex-col md:flex-row">
@@ -155,6 +174,12 @@ console.log(listings);
                             <ListingItem key={listing._id} listing={listing}/>
                         ))
                     }
+                    {showMore && (
+                        <button
+                        onClick= {onShowMoreClick}
+                        className="text-green-700 hover:underline p-7 text-center w-full"> Show more...
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
